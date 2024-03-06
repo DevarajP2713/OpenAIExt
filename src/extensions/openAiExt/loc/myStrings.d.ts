6c6b43fa97ef670ba6d482af122c1dd91fb919f4
DevarajP2713 <devarajupm7@gmail,com>
2024-03-06 10:41:48 +0530

    First Push

---
+++ src/extensions/openAiExt/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IOpenAiExtApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'OpenAiExtApplicationCustomizerStrings' {
+  const strings: IOpenAiExtApplicationCustomizerStrings;
+  export = strings;
+}
